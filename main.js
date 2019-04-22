f5b4a03dd7f3c43254f1de65e7f6ba5a7e0bd96b
Yaseen Shaikh <yaseen.nvt@gmail.com>
2019-04-21 20:15:40 -0400

    Initial stuff

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let screen = PIXI.screen;
+
+let app = new PIXI.Application();
+
+document.body.appendChild(app.view);
+
+class State {
+	constructor(num_particles) {
+		this.particles = [];
+	}
+}
+
+class PXObj {
+
+}
+
+class Blob extends PXObj {
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.
+	}
+}
